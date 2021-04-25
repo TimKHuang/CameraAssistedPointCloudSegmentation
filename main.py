b7e6cfdfae113819d578480d08a2953d342b9b24
TimKHuang <timkhuang@icloud.com>
2021-04-25 19:05:08 +0800

    extract visualisation part

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import os
 import cv2
+import numpy as np
 
 from preparation.odometryDataset import Dataset
+from projection.project import points_to_image_project
+from visualisation.image import show_image, plot_point_on_image
 
 # The Path to the downloaded odometry file
 calibration = os.path.join(os.path.pardir, "dataset", "calibration")
 color = os.path.join(os.path.pardir, "dataset", "color")
 velodyne = os.path.join(os.path.pardir, "dataset", "velodyne")
 
+# Field of view
+v_fov = (-24.9, 2.0)
+h_fov = (-45, 45)
+
 if __name__ == '__main__':
-    pass
+    with Dataset.open_odometry(calibration, color=color,  velodyne=velodyne) as dataset:
+        seq = dataset.get_sequence("00")
+        index = 0
+
+        img = np.array(seq.get_cam2(index))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        xyz_selected, xy_projected = points_to_image_project(seq, index, v_fov, h_fov)
+
+        show_image("Projected Image", plot_point_on_image(img, xyz_selected, xy_projected))
+
--- projection/project.py
+++ projection/project.py
@@ -3,11 +3,6 @@ import numpy as np
 from pykitti import odometry
 
 
-def depth_color(value, minimum=0, maximum=120):
-    value = np.clip(value, minimum, maximum)
-    return ((value - minimum) / (maximum - minimum) * 120).astype(np.uint8)
-
-
 def in_view_points(origin_points, v_fov, h_fov):
     x = origin_points[:, 0]
     y = origin_points[:, 1]
@@ -28,10 +23,7 @@ def in_view_points(origin_points, v_fov, h_fov):
     one = np.full((1, xyz.shape[1]), 1)
     xyz = np.vstack((xyz, one))
 
-    dist = np.sqrt(x ** 2 + y ** 2 + z ** 2)
-    dist_lim = dist[mask]
-    xyz_color = depth_color(dist_lim, 0, 70)
-    return xyz, xyz_color
+    return xyz
 
 
 def points_to_image_project(sequence, index, v_fov, h_fov):
@@ -49,45 +41,13 @@ def points_to_image_project(sequence, index, v_fov, h_fov):
     velo_to_camera = sequence.calib.T_cam2_velo
     camera_to_image = sequence.calib.K_cam2
 
-    xyz_velo, xyz_color = in_view_points(origin_points, v_fov, h_fov)
+    xyz_velo = in_view_points(origin_points, v_fov, h_fov)
 
     xyz_camera = np.matmul(velo_to_camera, xyz_velo)
     xyz_camera = np.delete(xyz_camera, 3, axis=0)
 
-    xyz_camera = np.matmul(camera_to_image, xyz_camera)
-
-    xy_image = xyz_camera[::] / xyz_camera[::][2]
+    xy_image = np.matmul(camera_to_image, xyz_camera)
+    xy_image = xy_image[::] / xy_image[::][2]
     xy_image = np.delete(xy_image, 2, axis=0)
 
-    return xyz_velo, xy_image, xyz_color
-
-
-if __name__ == '__main__':
-    import os
-    from preparation.odometryDataset import Dataset
-
-    calib = os.path.join(os.path.pardir, os.path.pardir, "dataset", "calibration")
-    color = os.path.join(os.path.pardir, os.path.pardir, "dataset", "color")
-    velodyne = os.path.join(os.path.pardir, os.path.pardir, "dataset", "velodyne")
-
-    with Dataset.open_odometry(calib, color=color, velodyne=velodyne) as dataset:
-        seq = dataset.get_sequence("00")
-        points, color = points_to_image_project(seq, 0, (-24.9, 2.0), (-45, 45))
-        print("COLOR!")
-        print(color)
-
-        img_np = np.array(seq.get_cam2(0))
-        img_cv2 = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
-
-        cv2.imshow("img", img_cv2)
-        cv2.waitKey(0)
-
-        hsv_image = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2HSV)
-        for i in range(points.shape[1]):
-            cv2.circle(hsv_image, (np.int32(points[0][i]), np.int32(points[1][i])), 2, (int(color[i]), 255, 255), -1)
-
-        final = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
-        cv2.imshow("img", final)
-        cv2.waitKey(0)
-
-        print("SUCCESS")
+    return xyz_velo, xy_image
